fbe6de5418b874a76f2400546ac0324e55c7834c
tomato <tomatomato2121@gmail.com>
2024-07-01 20:07:19 +0900

    adding link feature to the quick-access and adding introduce page

---
+++ app/introduce/page.tsx
@@ -0,0 +1,7 @@
+export default function IntroducePage() {
+    return (
+        <div className="h-full flex justify-center items-center">
+            Introduce Page
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar/sidebar";
-import MarkdownTest from "./marktest";
+
 
 export const metadata: Metadata = {
 	title: {
