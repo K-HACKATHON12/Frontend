9a07ca90809ab5feed15e18ba2b0cc051021bbf1
tomato <tomatomato2121@gmail.com>
2024-07-29 18:45:15 +0900

    adding modal for google map

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,8 +1,9 @@
 import { Prompt } from "@/components/chat/background";
 import { Chart } from "@/components/chart/chart";
+import { MapModal } from "@/components/modal";
 
 async function fetchData(url: string) {
-    const response = await fetch(url, { cache: 'force-cache' });
+	const response = await fetch(url, { cache: 'force-cache' });
 	if (!response.ok) {
 		throw new Error('Failed to fetch data');
 	}
@@ -14,7 +15,13 @@ export default async function Home() {
 
 	return (
 		<div className="flex flex-col mr-4 h-screen w-full">
-			<Chart expenditureData={expenditureData} />
+			<div className="w-full flex">
+				<Chart expenditureData={expenditureData} />
+				{/* <div className="w-64 h-auto ml-5 border-1 border-gray-300 rounded-xl mb-10 mt-2">
+					a
+				</div> */}
+				<MapModal />
+			</div>
 			<Prompt />
 		</div>
 	);
---
+++ components/modal.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import React from "react";
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    useDisclosure
+} from "@nextui-org/modal";
+import { Button } from "@nextui-org/react";
+import { FaMapMarkedAlt } from "react-icons/fa";
+
+export function MapModal() {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    return (
+        <>
+            <Button isIconOnly onPress={onOpen} className="w-64 h-auto ml-5 mb-10 mt-2 bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg text-9xl">
+                <FaMapMarkedAlt />
+            </Button>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+                            <ModalBody>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Nullam pulvinar risus non risus hendrerit venenatis.
+                                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Nullam pulvinar risus non risus hendrerit venenatis.
+                                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                                </p>
+                                <p>
+                                    Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
+                                    dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis.
+                                    Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod.
+                                    Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur
+                                    proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                                </p>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="danger" variant="light" onPress={onClose}>
+                                    Close
+                                </Button>
+                                <Button color="primary" onPress={onClose}>
+                                    Action
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+    );
+}
